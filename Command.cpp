81649062276ce7caee773ce5637ed13cfdb69fff
radek <radek@Radek-ubuntu>
2013-09-30 22:00:20 +0200

    Created base classes and structure for lowest-level. Now to implementation!

---
+++ Command.cpp
@@ -0,0 +1,15 @@
+/* 
+ * File:   Command.cpp
+ * Author: radek
+ * 
+ * Created on 28 wrzesień 2013, 20:12
+ */
+
+#include "Command.h"
+
+Command::Command(std::string code) {
+}
+
+Command::~Command() {
+}
+
